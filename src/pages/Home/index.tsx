5b9097ffbc87029504512e8ba200c26030880cc6
August-Souza <7luna.lunar@gmail.com>
2020-09-13 23:08:00 -0300

    Alt Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,15 +1,29 @@
 import React from 'react';
 import Player from '../../components/Player';
+import Sponsors from '../../components/Sponsors';
+import Footer from '../../components/Footer';
 
-import { Container, Content } from './styles';
+import imgLive from '../../assets/imgLogoR.png';
+
+import { Container, Live } from './styles';
 
 const Home: React.FC = () => {
   return (
     <>
       <Player />
       <Container>
-        <Content />
+        <Live>
+          <div>
+            <h2>Santos Charme Lives</h2>
+            <p>
+              Todos os domingos a partir das 20 horas LIVE! Santos Charme!!!
+            </p>
+          </div>
+          <img src={imgLive} alt="liveimg" />
+        </Live>
+        <Sponsors />
       </Container>
+      <Footer />
     </>
   );
 };
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -2,15 +2,37 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   display: flex;
+  flex-direction: column;
   justify-content: center;
-  max-width: 100%;
-  height: 1000px;
 `;
 
-export const Content = styled.div`
+export const Live = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
   margin-top: 30px;
-  max-width: 1200px;
   width: 100%;
-  height: 100vh;
+  height: 400px;
   background-color: #363633;
+
+  div {
+    h2 {
+      color: #fff;
+      font-size: 50px;
+      font-weight: bold;
+    }
+
+    p {
+      margin-top: 20px;
+      color: #f6f9f6;
+      font-weight: lighter;
+    }
+  }
+
+  img {
+    width: 25%;
+    height: 35%;
+  }
 `;
+
+export const Sponsors = styled.div``;
